125ba92f7340f1ba681a2879b40958259f794d55
Marvin Heer <Marvin.Heer@outlook.de>
2019-10-31 17:15:23 +0100

    2

--- Beispielprojekt/Klassen.cpp
+++ Beispielprojekt/Klassen.cpp
@@ -1,24 +1,34 @@
 #include "stdafx.h"
 #include "Klassen.h"
 
-class Spielfeld
-{
 
-	Gosu::Image bild;
-	std::array<std::array<Feld, SPIELFELD_BREITE>, SPIELFELD_BREITE> zustand;
-	int64_t score=0;
 
-public:
-	uint16_t hoehe() {
+	uint16_t Spielfeld::hoehe() {
 		return this->zustand.size();
 	}
-	void Spielreset() {
+	void Spielfeld::reset() {
 		for (std::array<Feld,SPIELFELD_BREITE>& zeile_ref : this->zustand) {
 			for (Feld& elem_ref : zeile_ref) {
 				elem_ref.status = 0;
 			}
 		}
 		this->score = 0;
+		this->startzeit = time(NULL);
+		srand(time(NULL));
+	}
+	double Spielfeld::dauer() {
+		return difftime(time(NULL), this->startzeit);
 	}
 
-};
+	Spielstein::Spielstein() {
+		this->farbe = farben.at(rand()%farben.size());
+	}
+
+	bool AktiverSpielstein::platzieren() {
+
+	}
+	AktiverSpielstein::AktiverSpielstein(AktiverSpielstein const& kopie) {
+		*this = kopie;
+		this->positionAufSpielfeld.x = (SPIELFELD_BREITE / 2);
+		this->positionAufSpielfeld.y = (SPIELFELD_BREITE / 2);
+	}
--- Beispielprojekt/Klassen.h
+++ Beispielprojekt/Klassen.h
@@ -1,12 +1,23 @@
 #pragma once
+#include <cstdint>
 #include <Gosu/Image.hpp>
 #include <Gosu/Graphics.hpp>
 #include <ctime>
+#include <cstdlib>
+#include <array>
 
+
+
+std::vector < Gosu::Color > farben = { Gosu::Color::BLUE,Gosu::Color::BLUE,Gosu::Color::RED,Gosu::Color::GREEN,Gosu::Color::YELLOW };
+std::vector<std::array<std::array<bool, 4>, 4>> formen;
 const uint16_t SPIELFELD_BREITE=10;
+
 struct Feld {
 	bool status;
 	Gosu::Color farbe;
+};
+struct koord {
+	uint16_t x, y;
 };
 class Spielfeld
 {
@@ -15,8 +26,23 @@ class Spielfeld
 	std::array<std::array<Feld,SPIELFELD_BREITE>,SPIELFELD_BREITE> zustand;
 	int64_t score;
 	time_t startzeit;
+	koord refpos = {0,0};//obere linke Ecke
 public:
 	uint16_t hoehe();
 	void reset();
+	double dauer();//Zeit seit start in s
 
+};
+class Spielstein {
+	Gosu::Color farbe;
+
+public:
+	Spielstein();
+	void draw();
+};
+class AktiverSpielstein : public Spielstein {
+	koord positionAufSpielfeld;//Position von linker Ecke aus
+public:
+	AktiverSpielstein(AktiverSpielstein const& kopie);
+	bool platzieren();
 };
